bbea0d48f0d187580a23d13f12e67840ef90f635
ankit-sr <ankitrajput6622@gmail.com>
2022-05-03 13:04:53 +0530

    added url_for path for images and stylesheets

--- vroom/routes.py
+++ vroom/routes.py
@@ -5,3 +5,24 @@ from flask import render_template
 @app.route('/home')
 def home():
   return render_template('landing.html')
+
+@app.route('/dashboard')
+def dashboard():
+  return render_template('home.html')
+
+@app.route('/book-service')
+def book_service():
+  return render_template('book_service.html')
+
+@app.route('/signup')
+def signup():
+  return render_template('signup.html')
+
+@app.route('/login')
+def login():
+  return render_template('login.html')
+
+@app.route('/service-details')
+def service_details():
+  return render_template('service_details.html')
+
